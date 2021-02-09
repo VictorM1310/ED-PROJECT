4bdf6225e408de5760fccfdad023245398d0a949
Jesús Santiago Fragoso Quintal <66925466+santFragoso@users.noreply.github.com>
2021-02-09 00:38:22 -0600

    Update pruebaBFS.c

--- pruebaBFS.c
+++ pruebaBFS.c
@@ -7,6 +7,101 @@
 #define NVERTICES 1000
 #define NARISTAS 34629
 
+typedef struct
+{
+	int **B;
+	unsigned int ren;
+	unsigned int col;
+	unsigned int tama;
+
+} arregloInt2D;
+
+
+int initArregloInt2D(arregloInt2D *aI, unsigned int r1, unsigned int c1);
+
+void liberaArregloInt2D(arregloInt2D *aI);
+
+
+
+int initArregloInt2D(arregloInt2D *aI, unsigned int r1, unsigned int c1)
+{
+	if (r1 && c1) //Validamos que r y c sean diferentes de 0
+	{
+		unsigned int i;
+
+		aI->B = malloc(r1 * sizeof (int *));
+		if (aI->B != NULL) //equivalente a aI->A
+		{
+			aI->B[0] = malloc(r1 * c1 * sizeof (int));
+			if (aI->B[0] != NULL) //equivalente a aI->A[0]
+				for (i = 1; i < r1; ++i)
+					//aI->A[i] = &(aI->A[0][i * c]);//Aqui usamos el operador &
+					aI->B[i] = aI->B[0] + i * c1;//Aqui usamos aritmetica de apuntadores.
+			else
+			{
+				free(aI->B);
+				aI->B = NULL;
+				aI->ren = aI->col = aI->tama = 0;
+				return -1;
+			}
+		}
+		else
+		{
+			aI->ren = aI->col = aI->tama = 0;
+			return -1;
+		}
+		aI->ren = r1;
+		aI->col = c1;
+		aI->tama = r1 * c1;
+		return 0;
+	}
+	aI->ren = aI->col = aI->tama = 0;
+	aI->B = NULL;
+	return 0;
+}
+
+
+void liberaArregloInt2D(arregloInt2D *aI)
+{
+		if(aI->tama)
+		{
+			free(aI->B[0]);
+			free(aI->B);
+			aI->ren = aI->col = aI->tama = 0;
+			aI->B = NULL;
+		}
+}
+
+
+int contarVinculos(Grafo *G, int vertice);
+
+int contarVinculos(Grafo *G, int vertice)
+{
+	int valencia = 0;
+	
+	for (int i=0; i<vertice; ++i)
+	{	
+		if (G->M.Tabla[vertice][i] == 1)
+		++valencia;	
+	}
+	
+	for (int i=0; i<(G->M.nV - (vertice + 1)); ++i  )
+	{
+		if (G->M.Tabla[vertice+1+i][vertice] == 1)
+		++valencia;		
+	}
+	
+	return valencia;
+}
+
+
+
+
+
+
+
+
+
 int main(int argc, char**argv)
 {
 	Grafo G;
@@ -66,12 +161,35 @@ int main(int argc, char**argv)
 	printf("La ruta de %s a %s es: \n\n",nomUsuarioO,nomUsuarioD);
 
 	imprimeRuta(&G, nomUsuarioD, Tabla_BFS);
+	
+	
+	int r = NVERTICES;	
+	int c = 2;
+	
+	
+	
+	arregloInt2D listaValencias;
+	initArregloInt2D(&listaValencias, r, c );
+	
+	for (int i=0; i<NVERTICES; ++i)
+	{
+		listaValencias.B[i][0] = contarVinculos(&G, i); 
+		listaValencias.B[i][1] = i;
+	}
+	
+	
+	
+	
+	
 
    if (Tabla_BFS != NULL)
       free(Tabla_BFS);
 
-	return 0;
-
 	free(nomUsuarioD);
 	free(nomUsuarioO);
+	liberaArregloInt2D(&listaValencias);
+	
+	return 0;
+
+	
 }
